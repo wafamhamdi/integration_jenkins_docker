04d12d441588fbd6859f1a7d2e255cb4c12be13e
wafa <wafa.mhamdi.jammel@gmail.com>
2021-04-08 19:38:04 +0200

    first commit

---
+++ jenkins_docker_integration/src/main/java/com/example/jenkins_docker_integration/JenkinsDockerIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkins_docker_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDockerIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDockerIntegrationApplication.class, args);
+	}
+
+}
---
+++ jenkins_docker_integration/src/test/java/com/example/jenkins_docker_integration/JenkinsDockerIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkins_docker_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDockerIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
